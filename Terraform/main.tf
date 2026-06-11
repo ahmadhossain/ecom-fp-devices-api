b82aaab48a4a590636e63cda63ba662ad5a6f79a
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2026-06-11 22:06:00 +0600

    Added terraform

---
+++ Terraform/main.tf
@@ -0,0 +1,55 @@
+resource "aws_dynamodb_table" "devices" {
+  name         = var.table_name
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.project_name}-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [{
+      Effect = "Allow"
+
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "dynamodb_access" {
+  name = "${var.project_name}-ddb-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [{
+      Effect = "Allow"
+
+      Action = [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:Scan",
+        "dynamodb:Query"
+      ]
+
+      Resource = aws_dynamodb_table.devices.arn
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.dynamodb_access.arn
+}
